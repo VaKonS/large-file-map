46bba7bcf1a019be5882a5d85f1bc5d4edd1a106
Valentin <vakons@mail.ru>
2017-06-11 01:25:01 +0300

    Pixel-aligned bytes averaging
    Maps look more logical this way – structures are aligned, for instance.

--- large_file_map.lua
+++ large_file_map.lua
@@ -11,8 +11,11 @@ cmd:option('-size',          0, 'Data file size in bytes (if it was detected inc
 cmd:option('-image', 'out.png', 'Output image name.')
 cmd:option('-x',          1366, 'Image width.')
 cmd:option('-y',           768, 'Image height.')
+cmd:option('-u',            16, 'Update image every N lines (faster).')
 cmd:text()
 
+
+local params = cmd:parse(arg)
 
 local function plural_bytes(n)
   local b
@@ -21,7 +24,6 @@ local function plural_bytes(n)
 end
 
 
-local function main(params)
   if params.sd == '' then
     print("\nInput data file is not given.\n")
     os.exit()
@@ -44,10 +46,10 @@ local function main(params)
     end
   end
 
-  local cnt = params.x * params.y * 3
+  local cnt = params.x * params.y
   local cl = math.ceil(dl / cnt)
   if cl < 1 then cl = 1 end  -- if file has 0 length
-  print("File: \"" .. params.sd .. "\"\n" .. dl .. plural_bytes(dl) .. ", averaging length: " .. cl .. plural_bytes(cl) .. ".")
+  print("File: \"" .. params.sd .. "\"\n" .. dl .. plural_bytes(dl) .. ", 1 pixel = " .. cl .. plural_bytes(cl) .. ".")
 
   -- serialized torch.ByteTensor(cl) template without data, to not recalculate it every time
   local bt_tp_cl = string.sub(torch.serialize(torch.ByteTensor(cl), 'binary'), 1, (-1 - cl))
@@ -70,7 +72,6 @@ local function main(params)
 
   for y = 1, params.y do
   for x = 1, params.x do
-  for i = 1, 3 do
     local c = df:read(cl)
     if c ~= nil then
       local ser_tensor
@@ -79,18 +80,19 @@ local function main(params)
       else
         ser_tensor = string.sub(torch.serialize(torch.ByteTensor(#c), 'binary'), 1, (-1 - #c)) .. c
       end
-      img[i][y][x] = torch.deserialize(ser_tensor, 'binary'):double():mean() / 255
+      img[{{}, y, x}] = torch.deserialize(ser_tensor, 'binary'):view(1, -1):expand(3, #c):t():contiguous():view(3, -1):double():mean(2):div(255)
 
       ci = ci + 1
-      if (i == 1) and (x == 1) then
+      if (x == 1) then
         clock_current = os.clock()
         if clock_current < clock_previous then clock_round = clock_round + 1 end
         clock_previous = clock_current
         print(string.format('\027[1A\027[K\027[1000D%1.3f%%, %1.1f seconds remaining.', ci / cnt * 100, (clock_round * 4294.967296 + clock_current - clock_start) / ci * (cnt - ci)))
-        image.save(params.image, img)
+        if (y % params.u) == 0 then
+          image.save(params.image, img)
+        end
       end
     end
-  end
   end
   end
   clock_current = os.clock()
@@ -98,9 +100,7 @@ local function main(params)
   print(string.format('\027[1A\027[K\027[1000D100%%, %1.1f seconds.', (clock_round * 4294.967296 + clock_current - clock_start)))
 
   df:close()
+  print("Writing \"" .. params.image .. "\".\n")
   image.save(params.image, img:clamp(0, 1))
-end
 
-
-local params = cmd:parse(arg)
-main(params)
+  os.exit()
